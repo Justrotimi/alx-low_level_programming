fd9b3fa290540e5cf030002508671edb4981dbb9
Justrotimi <Bankoleoluwarotimi2020@gmail.com>
2022-07-05 16:13:06 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - printing alphabets
+ *
+ * Return: 0 (success)
+ */
+void print_alphabet(void)
+{
+	int f;
+
+	for (f = 97; f <= 122; f++)
+	{
+		_putchar(f);
+	}
+	       _putchar('\n');
+}
