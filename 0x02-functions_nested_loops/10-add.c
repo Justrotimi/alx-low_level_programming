88cae027dc7471ec54df1184a2ea9f5b8202a14c
Justrotimi <Bankoleoluwarotimi2020@gmail.com>
2022-07-06 20:52:59 -0700

    add c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - function that add two intergers and return result
+ * @a: first number
+ * @c: second number
+ *
+ * Return: Additionof first and second number
+ */
+int add(int a, int c)
+{
+	return (a + c);
+}
