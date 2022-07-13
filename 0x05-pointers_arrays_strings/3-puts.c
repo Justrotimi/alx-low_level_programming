5f21c4e85ae9a5e3c0b0a0dc082823e06ae435bf
Justrotimi <Bankoleoluwarotimi2020@gmail.com>
2022-07-13 03:57:37 -0700

    putsc

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,16 @@
 #include "main.h"
+/**
+ * _puts - function thst print a string
+ * @str: pointer that store value
+ *
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
