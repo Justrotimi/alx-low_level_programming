70d24d8f6cfbf11154f8e833f9217dda27610059
Justrotimi <Bankoleoluwarotimi2020@gmail.com>
2022-07-14 21:01:35 -0700

    encodes string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - functions that encodes string into 1337
+ * @n: pointer that stores character
+ *
+ * Return: character
+ */
+char *leet(char *n)
+{
+	int i, k;
+	char c[] = "aAaEoOt1L";
+	char t[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (k = 0; c[k] != '\0'; k++)
+		{
+			if (n[i] == c[k])
+			{
+				n[i] = t[k];
+			}
+		}
+	}
+	return (n);
+}
+
