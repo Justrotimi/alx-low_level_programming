ea82179bff9c15854f36d1418807b4de0e406153
Justrotimi <Bankoleoluwarotimi2020@gmail.com>
2022-07-20 12:44:22 -0700

    squareroot

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -19,3 +19,19 @@ int _sqrt(int n, int s_root)
 
 	return (_sqrt(n, s_root + 1));
 }
+
+/**
+ * _sqrt_recursion - natural square root
+ * @n: number to calculate square root
+ *
+ * Return: natural square root
+ */
+int _sqrt_recursion(int n)
+{
+
+	if (n < 0)
+		return (-1);
+	if (n == 1)
+		return (1);
+	return (_sqrt(n, 0));
+}
